fdfec31876566e8525f5a62b1ec2c3f2399ae4dc
An Nguyen <an.nguyentruong99@gmail.com>
2021-03-25 21:10:00 +0700

    Declared function to generate training data set

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-def main():
-  pass
+from random import randint
+import scikit_learn
+
+def generate_training_set() -> []:
+  data = []
+  for _ in range(100):
+    item = [randint(0, 1000) for _ in range(3)]
+    data.append(item)
+  return data
